410e8a576f0b8a033b07cc685df96588cd6b577b
RurikBTW <161582723+RurikBTW@users.noreply.github.com>
2024-06-07 10:18:09 +0300

    Update lab4-arch.cpp

--- lab4-arch.cpp
+++ lab4-arch.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <cstdlib> // For the system function
+#include <cstdlib> 
 
 using namespace std;
 
 int main() {
-    // Paths to the folders to be deleted
+
     const char* folderPath1 = "D:\\Konovalov\\lab3-konovalov\\PT-23-1\\Konovalov Ivan Denisovich\\batch\\Non-hidden-folder";
     const char* folderPath2 = "D:\\Konovalov\\lab3-konovalov\\PT-23-1\\Konovalov Ivan Denisovich\\batch\\Hidden-folder";
 
-    // Deleting the first folder
     string command1 = "rmdir /s /q \"" + string(folderPath1) + "\"";
     if (system(command1.c_str()) != 0) {
         cerr << "Error deleting folder: " << folderPath1 << endl;
@@ -17,7 +16,6 @@ int main() {
         cout << "Folder successfully deleted: " << folderPath1 << endl;
     }
 
-    // Deleting the second folder
     string command2 = "rmdir /s /q \"" + string(folderPath2) + "\"";
     if (system(command2.c_str()) != 0) {
         cerr << "Error deleting folder: " << folderPath2 << endl;
