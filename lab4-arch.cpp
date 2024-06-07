f8c9e2d273d86c580e97314a6bd496692097a693
RurikBTW <161582723+RurikBTW@users.noreply.github.com>
2024-06-07 10:17:52 +0300

    Update lab4-arch.cpp

--- lab4-arch.cpp
+++ lab4-arch.cpp
@@ -1,5 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib> // For the system function
+
+using namespace std;
 
 int main() {
     // Paths to the folders to be deleted
@@ -7,21 +9,21 @@ int main() {
     const char* folderPath2 = "D:\\Konovalov\\lab3-konovalov\\PT-23-1\\Konovalov Ivan Denisovich\\batch\\Hidden-folder";
 
     // Deleting the first folder
-    if (std::system(("rmdir /s /q \"" + std::string(folderPath1) + "\"").c_str()) != 0) {
-        std::cerr << "Error deleting folder: " << folderPath1 << std::endl;
+    string command1 = "rmdir /s /q \"" + string(folderPath1) + "\"";
+    if (system(command1.c_str()) != 0) {
+        cerr << "Error deleting folder: " << folderPath1 << endl;
         return 1;
-    }
-    else {
-        std::cout << "Folder successfully deleted: " << folderPath1 << std::endl;
+    } else {
+        cout << "Folder successfully deleted: " << folderPath1 << endl;
     }
 
     // Deleting the second folder
-    if (std::system(("rmdir /s /q \"" + std::string(folderPath2) + "\"").c_str()) != 0) {
-        std::cerr << "Error deleting folder: " << folderPath2 << std::endl;
+    string command2 = "rmdir /s /q \"" + string(folderPath2) + "\"";
+    if (system(command2.c_str()) != 0) {
+        cerr << "Error deleting folder: " << folderPath2 << endl;
         return 1;
-    }
-    else {
-        std::cout << "Folder successfully deleted: " << folderPath2 << std::endl;
+    } else {
+        cout << "Folder successfully deleted: " << folderPath2 << endl;
     }
 
     return 0;
